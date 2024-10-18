563fc29c00b7d3be3a60f65469db8ec3828fda56
tomsantos07 <xtomsantosx@live.com>
2024-10-18 13:30:33 -0300

    inicia a manipulacao do DOM

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,29 +1,50 @@
 class CalcController {
 
   constructor() {
-    this._displayCalc = "0";
+    this._locale = 'pt-BR';
+    this._displayCalc = document.querySelector('#display')
+    this._dateEl = document.querySelector('#data');
+    this._timeEl = document.querySelector('#hora');
     this._currentDate;
     this.initialize();
   }
 
   initialize() {
-
-    let displayCalcEl = document.querySelector('#display');
-    let dateEl = document.querySelector('#data');
-    let timeEl = document.querySelector('#hora');
     
+    setInterval(()=> {
+
+      this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+      this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+      
+    }, 1000)
+  }
+
+  get displayTime() {
+    return this._timeEl.innerHTML;
+  }
+
+  set displayTime(value) {
+    return this._timeEl.innerHTML = value;
+  }
+
+  get displayDate() {
+    return this._dateEl.innerHTML;
+  }
+
+  set displayDate(value) {
+    return this._dateEl.innerHTML = value;
   }
 
   get displayCalc() {    
     return this._displayCalc;
   }
 
-  set displayCalc(valor) {   
-    this._displayCalc = valor;
+  set displayCalc(value) {   
+    this._displayCalc = value;
   }
 
   get currentDate() {
-    return this._currentDate;
+    return new Date();
   }
 
   set currentDate(valor) {
