1faee7e828f39076649ed84cd5cef585533ed6cc
tomsantos07 <xtomsantosx@live.com>
2024-11-19 20:29:12 -0300

    add isNAN e array.length

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -47,13 +47,51 @@ class CalcController {
 
   }
 
+  getLastOperation() {
+    return this._operation[this._operation.length-1];
+  }
+
+  setLastOperation(value) {
+
+    this._operation[this._operation.length-1] = value
+
+  }
+
+  isOperator(value) {
+
+    return (['+', '-', '*', '%', '/'].indexOf(value) > -1);
+
+  }
+
   addOperation(value) {
 
-    this._operation = value;
+    console.log('A', isNaN(this.getLastOperation()));
+
+    if (isNaN(this.getLastOperation())) {
+
+      if (this.isOperator(value)) {
+
+        this._setLastOperation(value)
+        
+      } else if(isNaN(value)) {
+
+        // Outra coisa
+        console.log(value); 
+
+      } else {
+
+        this._operation.push(value);
+
+      }
+
+    } else {
+      let newValue = this.getLastOperation().toString() + value.toString();
+      this.setLastOperation(parseInt(newValue));
+    }
 
     console.log(this._operation);
     
-
+    
   }
 
   setError() {
@@ -62,9 +100,7 @@ class CalcController {
 
   }
 
-  execBtn(value) {
-    console.log('case', value);
-    
+  execBtn(value) {  
 
     switch (value) {
 
@@ -77,24 +113,30 @@ class CalcController {
         break;
 
       case 'soma':
+        this.addOperation('+');
         break;
 
       case 'subtracao':
+        this.addOperation('-');
         break;
 
       case 'divisao':
+        this.addOperation('/');
         break;
 
       case 'multiplicacao':
+        this.addOperation('*');
         break;
 
       case 'porcento':
+        this.addOperation('%');
         break;
 
       case 'igual':
         break;
 
       case 'ponto':
+        this.addOperation('.');
         break;
 
       case '0':
@@ -126,8 +168,6 @@ class CalcController {
       this.addEventListenerAll(btn, "click drag", e => {
 
         let textBtn = btn.className.baseVal.replace("btn-", ""); 
-
-        console.log(textBtn);
         
         this.execBtn(textBtn);
 
